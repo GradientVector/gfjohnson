6be7c5e17e5398fcbafdb5671da5f45582760044
John Harvey <thegradientvector@gmail.com>
2013-02-21 21:50:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfjohnson::Application.config.secret_token = 'b01e48b0382d9a33afe4c95709124e0cad78d93ca0fa2ba4d38b667071ae890576d7eedad33450ddca5c4364326f5bcf6c11db2d4418d218138fe6e7d5b27a87'
