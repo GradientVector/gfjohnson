390aa15e2fd1f2270d85040add49e0d93ffe1e45
John Harvey <thegradientvector@gmail.com>
2013-05-24 14:22:10 -0500

    Added tests for soft-delete of PrivateLessonPackageTypes.

--- app/models/private_lesson_package_type.rb
+++ app/models/private_lesson_package_type.rb
@@ -1,7 +1,12 @@
 class PrivateLessonPackageType < ActiveRecord::Base
   attr_accessible :cost_in_dollars, :duration_in_minutes, :name
 
+  after_initialize do
+    self.is_active ||= true
+  end
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :duration_in_minutes, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :cost_in_dollars, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :is_active, inclusion: { in: [true, false] }
 end
--- spec/requests/private_lesson_package_type_pages_spec.rb
+++ spec/requests/private_lesson_package_type_pages_spec.rb
@@ -14,5 +14,26 @@ describe "PrivateLessonPackageTypePages" do
 
     it { should have_selector("title", text: "All private lesson package types") }
     it { should have_selector("h1", text: "All private lesson package types") }
+
+    it { should_not have_link("delete") }
+
+    describe "delete links" do
+      let!(:one_lesson_package) { FactoryGirl.create(:private_lesson_package_type) }
+      before do
+        visit private_lesson_package_types_path
+      end
+      
+      it { should have_link("delete", href: private_lesson_package_type_path(one_lesson_package)) }
+      it "should only soft-delete a private lesson package type" do
+        expect { click_link "delete" }.not_to change(PrivateLessonPackageType, :count)
+      end
+
+      describe "after clicking link" do
+        before do
+          click_link "delete"
+        end
+        it { should_not have_link("delete", href: private_lesson_package_type_path(one_lesson_package)) }
+      end
+    end
   end
 end
